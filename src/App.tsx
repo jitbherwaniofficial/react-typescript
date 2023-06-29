806e1472926f3cf80945d2a86f5fbbf9f03ef088
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-06-29 17:05:08 +0530

    optimize app by giving Lazy Loading and Suspense Fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
-import Button from "./component/Button";
 import CounterProvider from "./provider/Counter";
+const Button = React.lazy(() => import("./component/Button"));
 
 function App() {
   return (
     <div className="App">
       <h1>Hello TypeScript</h1>
       <CounterProvider>
-        <Button text="Click Me!" />
+        <Suspense fallback={<p>This is Loading...</p>}>
+          <Button text="Click Me!" />
+        </Suspense>
       </CounterProvider>
     </div>
   );
